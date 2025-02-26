a8b01f1c412e828908ab3fc7b1f069dd02a96fae
Першин Максим Дмитриевич <mdpershin@edu.hse.ru>
2025-02-27 00:48:20 +0300

    added function to rerender web sith a form by JS.

--- venv/main.py
+++ venv/main.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from src import resume_manipulator
 
 app = Flask(__name__, static_url_path="", static_folder="templates")
 @app.route("/resume/generate", methods=["POST"])
+def submit():
+    form_data = {"name": request.form.get("id")}
+    updated_resume = resume_manipulator.generate_resume(data=form_data)
+    return updated_resume
 @app.route("/")
 def get_resume():
     resume_manipulator.generate_resume()
--- venv/src/resume_manipulator.py
+++ venv/src/resume_manipulator.py
@@ -1,20 +1,25 @@
 from datetime import datetime
 from jinja2 import Template
 
+
 TEMPLATE_FILE_NAME = "./templates/index.html"
 INDEX_FILE_NAME    = "./public/index.html"
 
 
-def generate_resume():
+def generate_resume(data=None  ):
     with open(TEMPLATE_FILE_NAME, "r", encoding="utf-8") as template_file:
         template_text = template_file.read()
         jinja_template = Template(template_text)
-        rendered_resume = jinja_template.render(
-            name="Max",
-            job_position = "Python Developer",
-            work_place = "Yandex",
-            datetime = datetime.now()
-        )
+        default_data = {
+            "name" : "Max",
+            "job_position" : "Python Developer",
+            "work_place" : "Yandex",
+            "datetime" : datetime.now()
+        }
+        if data:
+            default_data.update(data)
+
+        rendered_resume = jinja_template.render(default_data)
         with open(INDEX_FILE_NAME, "w", encoding="utf-8") as resume_file:
             resume_file.write(rendered_resume)
 
