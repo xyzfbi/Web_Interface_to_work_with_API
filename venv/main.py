8dfafaeeef13b3cff1b8eb1bae2d5085735e19a3
Першин Максим Дмитриевич <mdpershin@edu.hse.ru>
2025-02-27 00:48:20 +0300

    added function to rerender web sith a form by JS.

--- venv/main.py
+++ venv/main.py
@@ -1,14 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 from src import resume_manipulator
 
 app = Flask(__name__, static_url_path="", static_folder="templates")
-
 @app.route("/resume/generate", methods=["POST"])
-def generate_resume():
-    data = request.get_json()
-    resume_manipulator.generate_resume(data)
-    return jsonify({"status": "success", "message": "Resume generated"})
-
+def submit():
+    form_data = {"name": request.form.get("id")}
+    updated_resume = resume_manipulator.generate_resume(data=form_data)
+    return updated_resume
 @app.route("/")
 def get_resume():
     resume_manipulator.generate_resume()
