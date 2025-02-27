b5d1eb2008363eaa97f66ee487aeaf4177484572
Першин Максим Дмитриевич <mdpershin@edu.hse.ru>
2025-02-27 23:16:46 +0300

    created extra file json to storage data

--- venv/main.py
+++ venv/main.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify
 from src import resume_manipulator
 
 app = Flask(__name__, static_url_path="", static_folder="templates")
+
 @app.route("/resume/generate", methods=["POST"])
-def submit():
-    form_data = {"name": request.form.get("id")}
-    updated_resume = resume_manipulator.generate_resume(data=form_data)
-    return updated_resume
+def generate_resume():
+    data = request.get_json()
+    resume_manipulator.generate_resume(data)
+    return jsonify({"status": "success", "message": "Resume generated"})
+
 @app.route("/")
 def get_resume():
     resume_manipulator.generate_resume()
